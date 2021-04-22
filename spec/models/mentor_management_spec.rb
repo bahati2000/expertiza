3727554949b011389bfbe96b904c7f61df23f84e
Bahati Wanza <bahati2000@msn.com>
2021-04-22 18:31:34 -0400

    created a new file for test mentor_management_spec.rb

---
+++ spec/models/mentor_management_spec.rb
@@ -0,0 +1,27 @@
+describe MentorManagement do
+
+  describe "#select_mentor" do
+
+  end
+
+  describe "#update_mentor_state" do
+
+  end
+
+  describe "#user_mentor?" do
+    expect(participant.name).to eq("mentor")
+  end
+
+  describe "#get_all_mentors" do
+
+  end
+
+  describe "#get_mentors_for_assignment" do
+
+  end
+
+  describe "#zip_mentors_with_team_count" do
+
+  end
+
+end
